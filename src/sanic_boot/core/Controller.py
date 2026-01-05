356c5b388c90565cb00e076b0f0cac1f7961122f
嬴寒 <yinghan22@163.com>
2026-01-05 09:05:19 +0800

    逻辑优化与代码质量优化

--- src/sanic_boot/core/Controller.py
+++ src/sanic_boot/core/Controller.py
@@ -4,8 +4,8 @@
 @email   : yinghan22@163.com
 @create  : 2025/12/3 16:12
 """
-
 import re
+from typing import Optional
 
 
 def __insert_space__(text):
@@ -16,34 +16,21 @@ def __to_kebab_case__(text):
     return __insert_space__(text).lower().replace(" ", "-")
 
 
-def Controller(*args, **kwargs):  # -> Callable[..., Any]:
+def Controller(target: Optional[type] = None, uri: Optional[str] = None):
     """
     Controller 控制器注册
 
-    :param kwargs['uri'] {str} 命名参数，指定 Controller 的路由路径
+    :param target {class} 注解对象
+    :param uri {str} 命名参数，指定 Controller 的路由路径
     """
-    if (len(args) == 1 and type(args[0]) is str) or "uri" in kwargs:
-        url: str = ""
-        if len(args) == 1:
-            uri = args[0]
-        if "uri" in kwargs:
-            uri = kwargs.get("uri")
 
-        def decoratorWithUri(cls):
-            nonlocal uri
-            setattr(cls, "__controller__", uri)
-            return cls
+    def wrapper(clazz: Optional[type], url: Optional[str] = None):
+        assert clazz is not None
+        if url is None:
+            url = clazz.__name__
+            url = __to_kebab_case__(url[: -len("Controller")] if url.endswith("Controller") else url)
+        assert url is not None
+        setattr(clazz, "__controller__", url)
+        return clazz
 
-        return decoratorWithUri
-    # else:
-    elif callable(args[0]):
-        cls = args[0]
-        uri = str(cls.__name__.replace("Controller", ""))
-        uri = __to_kebab_case__(uri)
-        setattr(cls, "__controller__", uri)
-        return cls
-
-    def decoratorNoting(cls):
-        return cls
-
-    return decoratorNoting
+    return wrapper if target is None else wrapper(target, url=uri)
