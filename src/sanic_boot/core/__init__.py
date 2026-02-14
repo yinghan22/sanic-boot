51abb9f434ad785452c847053f30ef965e55f1d2
嬴寒 <yinghan22@163.com>
2026-02-14 16:48:27 +0800

    add CRUD export to core module

--- src/sanic_boot/core/__init__.py
+++ src/sanic_boot/core/__init__.py
@@ -1,3 +1,19 @@
 from .Controller import Controller
 from .RequestMapping import *
 from .Task import Task
+from .crud import CRUD
+
+__all__ = [
+    "Controller",
+    "RequestMapping",
+    "GetMapping",
+    "PutMapping",
+    "PostMapping",
+    "DeleteMapping",
+    "HeadMapping",
+    "WebSocket",
+    "Websocket",
+    "websocket",
+    "Task",
+    "CRUD",
+]
