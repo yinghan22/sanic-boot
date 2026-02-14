1e1e003cbdae7c8f1f246eaf4cfa164e214a4aef
嬴寒 <yinghan22@163.com>
2026-02-14 16:47:55 +0800

    fix: improve Controller decorator parameter handling

--- src/sanic_boot/core/Controller.py
+++ src/sanic_boot/core/Controller.py
@@ -4,6 +4,7 @@
 @email   : yinghan22@163.com
 @create  : 2025/12/3 16:12
 """
+
 import re
 from typing import Optional
 
@@ -24,13 +25,16 @@ def Controller(target: Optional[type] = None, uri: Optional[str] = None):
     :param uri {str} 命名参数，指定 Controller 的路由路径
     """
 
-    def wrapper(clazz: Optional[type], url: Optional[str] = None):
+    def wrapper(clazz: Optional[type], inner_url: Optional[str] = None):
+        nonlocal uri
         assert clazz is not None
-        if url is None:
-            url = clazz.__name__
-            url = __to_kebab_case__(url[: -len("Controller")] if url.endswith("Controller") else url)
-        assert url is not None
-        setattr(clazz, "__controller__", url)
+        if uri is None:
+            inner_url = clazz.__name__
+            inner_url = __to_kebab_case__(inner_url[: -len("Controller")] if inner_url.endswith("Controller") else url)
+        else:
+            inner_url = uri
+        assert inner_url is not None
+        setattr(clazz, "__controller__", inner_url)
         return clazz
 
-    return wrapper if target is None else wrapper(target, url=uri)
+    return wrapper if target is None else wrapper(target, inner_url=uri)
