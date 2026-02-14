946dcb60fb710915d44443ac0e4b9a66f3d427c5
嬴寒 <yinghan22@163.com>
2026-02-14 16:48:51 +0800

    add Result.success method for standardized API response

---
+++ src/sanic_boot/core/Result.py
@@ -0,0 +1,21 @@
+"""
+@file    : Result.py
+@author  : yingHan/嬴寒
+@email   : yinghan22@163.com
+@create  : 2026/02/14 14:42
+"""
+from sanic import json
+
+
+class Result:
+    class Status:
+        success = 200
+        server_error = 500
+
+    @staticmethod
+    def success(data, message: str = None):
+        return json({
+            'status': Result.Status.success,
+            'data': data,
+            'message': message,
+        })
